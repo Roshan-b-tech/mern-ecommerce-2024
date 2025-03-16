d516be888b880bd736c17abd3f2e5bf1100255da
Roshan <roshangehlot500@gmail.com>
2025-03-16 12:58:07 +0530

    mm

--- client/src/store/shop/review-slice/index.js
+++ client/src/store/shop/review-slice/index.js
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+import axiosInstance from "@/config/axios";
 
 const initialState = {
   isLoading: false,
@@ -9,8 +9,8 @@ const initialState = {
 export const addReview = createAsyncThunk(
   "/order/addReview",
   async (formdata) => {
-    const response = await axios.post(
-      `http://localhost:5000/api/shop/review/add`,
+    const response = await axiosInstance.post(
+      `/api/shop/review/add`,
       formdata
     );
 
@@ -19,8 +19,8 @@ export const addReview = createAsyncThunk(
 );
 
 export const getReviews = createAsyncThunk("/order/getReviews", async (id) => {
-  const response = await axios.get(
-    `http://localhost:5000/api/shop/review/${id}`
+  const response = await axiosInstance.get(
+    `/api/shop/review/${id}`
   );
 
   return response.data;
@@ -42,6 +42,18 @@ const reviewSlice = createSlice({
       .addCase(getReviews.rejected, (state) => {
         state.isLoading = false;
         state.reviews = [];
+      })
+      .addCase(addReview.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(addReview.fulfilled, (state, action) => {
+        state.isLoading = false;
+        if (action.payload.data) {
+          state.reviews = action.payload.data;
+        }
+      })
+      .addCase(addReview.rejected, (state) => {
+        state.isLoading = false;
       });
   },
 });
