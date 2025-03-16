460fbe93144fa4d6310e04adfdd956743d86ef60
Roshan <roshangehlot500@gmail.com>
2025-03-16 12:52:19 +0530

    mm

--- client/src/store/shop/order-slice/index.js
+++ client/src/store/shop/order-slice/index.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import axios from "axios";
+import axiosInstance from "@/config/axios";
 
 const initialState = {
   approvalURL: null,
@@ -12,8 +12,8 @@ const initialState = {
 export const createNewOrder = createAsyncThunk(
   "/order/createNewOrder",
   async (orderData) => {
-    const response = await axios.post(
-      "http://localhost:5000/api/shop/order/create",
+    const response = await axiosInstance.post(
+      "/api/shop/order/create",
       orderData
     );
 
@@ -24,8 +24,8 @@ export const createNewOrder = createAsyncThunk(
 export const capturePayment = createAsyncThunk(
   "/order/capturePayment",
   async ({ paymentId, payerId, orderId }) => {
-    const response = await axios.post(
-      "http://localhost:5000/api/shop/order/capture",
+    const response = await axiosInstance.post(
+      "/api/shop/order/capture",
       {
         paymentId,
         payerId,
@@ -40,8 +40,8 @@ export const capturePayment = createAsyncThunk(
 export const getAllOrdersByUserId = createAsyncThunk(
   "/order/getAllOrdersByUserId",
   async (userId) => {
-    const response = await axios.get(
-      `http://localhost:5000/api/shop/order/list/${userId}`
+    const response = await axiosInstance.get(
+      `/api/shop/order/list/${userId}`
     );
 
     return response.data;
@@ -51,8 +51,8 @@ export const getAllOrdersByUserId = createAsyncThunk(
 export const getOrderDetails = createAsyncThunk(
   "/order/getOrderDetails",
   async (id) => {
-    const response = await axios.get(
-      `http://localhost:5000/api/shop/order/details/${id}`
+    const response = await axiosInstance.get(
+      `/api/shop/order/details/${id}`
     );
 
     return response.data;
